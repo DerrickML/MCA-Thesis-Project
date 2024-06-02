0854a27f0d997c280ad3aacc08e5c9d79cef8751
DeroMal <derrrickmal123@gmail.com>
2024-06-02 12:04:11 -0400

    aligninh

--- db.js
+++ db.js
@@ -5,10 +5,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    host: 'localhost',
+    user:'derrickml_derrickml',
+    password: '@mcaproject',
+    database: 'derrickml_mca_project'
 });
 
 module.exports = pool.promise();
